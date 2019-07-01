03ef76c4148d6a2ed1bf190a41dfec2acd833bb9
onkabb <shalaev.ilia@gmail.com>
2019-07-02 01:38:32 +0700

    hometask_2 complete 3

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -1,5 +1,6 @@
 package ru.skillbranch.devintensive.models
 
+
 import ru.skillbranch.devintensive.utils.Utils
 import java.util.*
 
@@ -62,58 +63,25 @@ data class User(
         }
     }
 
-    class Builder() {
-        private var id: String? = null
-        private var firstName: String? = null
-        private var lastName: String? = null
-        private var avatar: String? = null
-        private var rating: Int = 0
-        private var respect: Int = 0
-        private var lastVisit: Date? = null
-        private var isOnline: Boolean = false
-
-        fun id(value: String) {
-            id = value
-        }
-
-        fun firstName(value: String) {
-            firstName = value
-        }
-
-        fun lastName(value: String) {
-            lastName = value
-        }
-
-        fun avatar(value: String) {
-            avatar = value
-        }
-
-        fun rating(value: Int) {
-            rating = value
-        }
-
-        fun respect(value: Int) {
-            respect = value
-        }
-
-        fun lastVisit(value: Date) {
-            lastVisit = value
-        }
-
-        fun isOnline(value: Boolean) {
-            isOnline = value
-        }
-
-        fun build() = User(
-            id ?: (++lastId).toString(),
-            firstName,
-            lastName,
-            avatar,
-            rating,
-            respect,
-            lastVisit,
-            isOnline
-        )
+    data class Builder(
+        var id: String = "",
+        var firstName: String? = null,
+        var lastName: String? = null,
+        var avatar: String? = null,
+        var rating: Int = 0,
+        var respect: Int = 0,
+        var lastVisit: Date? = null,
+        var isOnline: Boolean = false
+    ) {
+        fun id(id: String) = apply { this.id = id }
+        fun firstName(firstName: String) = apply { this.firstName = firstName }
+        fun lastName(lastName: String) = apply { this.lastName = lastName }
+        fun avatar(avatar: String) = apply { this.avatar = avatar }
+        fun rating(rating: Int) = apply { this.rating = rating }
+        fun respect(respect: Int) = apply { this.respect = respect }
+        fun lastVisit(lastVisit: Date) = apply { this.lastVisit = lastVisit }
+        fun isOnline(isOnline: Boolean) = apply { this.isOnline = isOnline }
+        fun build() = User(id, firstName, lastName, avatar, rating, respect, lastVisit, isOnline)
     }
 }
 
